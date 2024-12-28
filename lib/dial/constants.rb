8909d8184279cd4c41c48f07e4e1ceb80a2f6dd4
Joshua Young <djry1999@gmail.com>
2024-12-28 12:26:01 +1000

    rename profile out directory

--- lib/dial/constants.rb
+++ lib/dial/constants.rb
@@ -6,7 +6,7 @@ module Dial
   REQUEST_TIMING_HEADER = "dial_request_timing"
 
   PROFILE_OUT_STALE_SECONDS = 60 * 60
-  PROFILE_OUT_RELATIVE_DIRNAME = "tmp/dial/profile/"
+  PROFILE_OUT_RELATIVE_DIRNAME = "tmp/dial/profiles/"
 
   PROSOPITE_IGNORE_QUERIES = [/schema_migrations/]
   PROSOPITE_LOG_RELATIVE_PATHNAME = "log/dial/prosopite.log"
